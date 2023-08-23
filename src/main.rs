e9afff010678e312456e2eeb4d6a50d46efdbb85
georgewoodall82 <georgewoodall82@gmail.com>
2023-08-23 13:33:09 +0100

    changed version to 1.0.0 added command line options (sort, order, category) terminal output now looks alot nicer Code is a bit neater

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,145 @@
 //tbitsearch - A torrent search engine that scrapes bitsearch for results.
 
+use std::rc::Rc;
+
+use clap::Parser;
+use colored::Colorize;
+use markup5ever::rcdom::Node;
 use reqwest::Url;
 use soup::prelude::*;
 use text_io::read;
 
-fn main() {
-    //Get program args
-    let args: Vec<String> = std::env::args().collect();
-    //Merge all args into one string, except the first
-    let mut query = args[1..].join(" ");
+#[derive(Parser)]
+struct Cli {
+    /// The query to search for
+    //#[arg(short, long)]
+    query: String,
 
-    //if no args were provided
-    if query.is_empty() {
-        print!("Bitsearch Search:");
-        query = read!("{}\n");
+    /// Sort by (relevance, seeders, leechers, data, size)
+    #[arg(short, long, default_value = "relevance")]
+    sort: String,
+
+    /// The sort order (asc, desc)
+    #[arg(short, long, default_value = "desc")]
+    order: String,
+
+    /// The category to search in (all, videos, software, music, games)
+    #[arg(short, long, default_value = "all")]
+    category: String,
+}
+
+struct Torrent {
+    name: String,
+    magnet: String,
+    torrent: String,
+    leechers: String,
+    seeders: String,
+    size: String,
+    date: String,
+}
+
+fn result_to_torrent(result: Rc<Node>) -> Option<Torrent> {
+    let magnet = result.tag("a").class("dl-magnet").find();
+
+    // If there is no magnet link, that means it has found a non-torrent, like an advert.
+    if magnet.is_none() {
+        return None;
     }
+    let name = result
+        .tag("h5")
+        .class("title")
+        .find()
+        .unwrap()
+        .tag("a")
+        .find()
+        .unwrap()
+        .text();
+    let magnet = magnet.unwrap().get("href").unwrap();
+    let torrent = result
+        .tag("a")
+        .class("dl-torrent")
+        .find()
+        .unwrap()
+        .get("href")
+        .unwrap();
+    let seeders = result
+        .tag("img")
+        .attr("alt", "Seeder")
+        .find()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .tag("font")
+        .find()
+        .unwrap()
+        .text();
+    let size = result
+        .tag("img")
+        .attr("alt", "Size")
+        .find()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .text();
+    let date = result
+        .tag("img")
+        .attr("alt", "Date")
+        .find()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .text();
+    let leechers = result
+        .tag("img")
+        .attr("alt", "Leecher")
+        .find()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .tag("font")
+        .find()
+        .unwrap()
+        .text();
 
-    if query.is_empty() {
+    Some(Torrent {
+        name,
+        magnet,
+        torrent,
+        leechers,
+        seeders,
+        size,
+        date,
+    })
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    if args.query.is_empty() {
         return;
     }
 
     let mut page = 1;
 
+    let category = match args.category.as_str() {
+        "all" => ("", ""),
+        "videos" => ("1", "2"),
+        "software" => ("5", "1"),
+        "music" => ("7", ""),
+        "games" => ("6", "1"),
+        _ => ("", ""),
+    };
+
     //url encode the query
     let mut url = Url::parse("https://bitsearch.to/search").unwrap();
-    url.set_query(Some(format!("q={}", query).as_str()));
+    url.set_query(Some(
+        format!(
+            "q={}&sort={}&order={}&category={}&subcat={}",
+            args.query, args.sort, args.order, category.0, category.1
+        )
+        .as_str(),
+    ));
+    println!("{}", url.as_str());
 
     let mut numresults: i32 = 0;
     let mut numpages: i32 = 0;
@@ -55,6 +170,7 @@ fn main() {
         } else {
             numresults
         };
+
         numpages = if page == 1 {
             (numresults as f32 / 20.0).floor() as i32
         } else {
@@ -65,90 +181,13 @@ fn main() {
             numpages = 1;
         }
 
-        struct Torrent {
-            name: String,
-            magnet: String,
-            torrent: String,
-            leechers: String,
-            seeders: String,
-            size: String,
-            date: String,
-        }
-
         let mut parsed: Vec<Torrent> = Vec::new();
 
         for result in results {
-            let magnet = result.tag("a").class("dl-magnet").find();
-
-            // If there is no magnet link, that means it has found an advert, so skip.
-            if magnet.is_none() {
-                continue;
+            match result_to_torrent(result) {
+                Some(torrent) => parsed.push(torrent),
+                None => continue,
             }
-
-            let name = result
-                .tag("h5")
-                .class("title")
-                .find()
-                .unwrap()
-                .tag("a")
-                .find()
-                .unwrap()
-                .text();
-            let magnet = magnet.unwrap().get("href").unwrap();
-            let torrent = result
-                .tag("a")
-                .class("dl-torrent")
-                .find()
-                .unwrap()
-                .get("href")
-                .unwrap();
-            let seeders = result
-                .tag("img")
-                .attr("alt", "Seeder")
-                .find()
-                .unwrap()
-                .parent()
-                .unwrap()
-                .tag("font")
-                .find()
-                .unwrap()
-                .text();
-            let size = result
-                .tag("img")
-                .attr("alt", "Size")
-                .find()
-                .unwrap()
-                .parent()
-                .unwrap()
-                .text();
-            let date = result
-                .tag("img")
-                .attr("alt", "Date")
-                .find()
-                .unwrap()
-                .parent()
-                .unwrap()
-                .text();
-            let leechers = result
-                .tag("img")
-                .attr("alt", "Leecher")
-                .find()
-                .unwrap()
-                .parent()
-                .unwrap()
-                .tag("font")
-                .find()
-                .unwrap()
-                .text();
-            parsed.push(Torrent {
-                name: name,
-                magnet: magnet,
-                torrent: Url::parse(torrent.as_str()).unwrap().try_into().unwrap(),
-                seeders: seeders,
-                size: size,
-                date: date,
-                leechers: leechers,
-            });
         }
 
         if parsed.len() == 0 {
@@ -157,11 +196,18 @@ fn main() {
         }
 
         for (i, torrent) in parsed.iter().enumerate() {
-            println!("{}: {}", i + 1, torrent.name);
+            println!(
+                "{}: {}",
+                (i + 1).to_string().bold(),
+                torrent.name.underline()
+            );
 
             println!(
                 "Seeders: {} | Leechers: {} | Size: {} | Date: {}\n",
-                torrent.seeders, torrent.leechers, torrent.size, torrent.date
+                torrent.seeders.bold(),
+                torrent.leechers.bold(),
+                torrent.size.bold(),
+                torrent.date.bold()
             );
         }
 
@@ -172,7 +218,6 @@ fn main() {
 
         match temp {
             Ok(num) => {
-
                 if (num.overflowing_sub(1).0) >= parsed.len() {
                     continue;
                 }
@@ -189,7 +234,13 @@ fn main() {
                     continue;
                 }
                 page += 1;
-                url.set_query(Some(format!("q={}&page={}", query, page).as_str()));
+                url.set_query(Some(
+                    format!(
+                        "q={}&page={}&sort={}&order={}&category={}&subcat={}",
+                        args.query, page, args.sort, args.order, category.0, category.1
+                    )
+                    .as_str(),
+                ));
                 continue;
             }
         }
